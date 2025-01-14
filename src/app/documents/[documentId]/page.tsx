291492c8f1b1ba12497d1bbbe97dec0b901d30bf
Suraj <suraj.ukumar.p@gmail.com>
2025-01-14 23:39:58 +0530

    core: editor store functionality

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
 import { Editor } from "./editor";
+import { Toolbar } from "./toolbar";
 
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
@@ -8,8 +9,8 @@ const DocumentIdPageProps = async ({ params }: DocumentIdPageProps) => {
   const { documentId } = await params;
 
   return (
-    <div>
-      Document ID: {documentId}
+    <div className="min-h-screen bg-[rgb(250,251,253)]">
+      <Toolbar />
       <Editor />
     </div>
   );
